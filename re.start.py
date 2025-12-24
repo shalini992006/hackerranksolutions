da363e04dde87103d51d549f349750288258830e
shalini992006 <shalini992006@gmail.com>
2025-12-24 20:48:15 +0530

    Add files via upload

---
+++ re.start.py
@@ -0,0 +1,9 @@
+import re
+S = input()
+k = input()
+matches = list(re.finditer(r'(?={})'.format(k), S))
+if matches:
+    for m in matches:
+        print((m.start(), m.start() + len(k) - 1))
+else:
+    print((-1, -1))
