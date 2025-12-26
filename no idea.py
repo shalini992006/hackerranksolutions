49ad5a6bee4cb54d49f44e97a1773288efee85b3
shalini992006 <shalini992006@gmail.com>
2025-12-26 20:28:18 +0530

    no idea.py

---
+++ no idea.py
@@ -0,0 +1,12 @@
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+A = set(map(int, input().split()))
+B = set(map(int, input().split()))
+happiness = 0
+for x in arr:
+   if x in A:
+        happiness += 1
+   elif x in B:
+        happiness -= 1
+print(happiness) 
+       
