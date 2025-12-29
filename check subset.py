a6a07f53fbae7ff93c2edd4a45ecb42b8e25214d
shalini992006 <shalini992006@gmail.com>
2025-12-29 19:04:52 +0530

    check subset.py

---
+++ check subset.py
@@ -0,0 +1,7 @@
+T = int(input())
+for _ in range(T):
+    n = int(input())
+    A = set(map(int, input().split()))
+    m = int(input())
+    B = set(map(int, input().split()))
+    print(A.issubset(B))
