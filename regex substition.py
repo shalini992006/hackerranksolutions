a3b1f20ccbc7d5ed08a3d85dc9e0c48e2ef23951
shalini992006 <shalini992006@gmail.com>
2025-12-31 19:33:48 +0530

    regex substition.py

---
+++ regex substition.py
@@ -0,0 +1,7 @@
+import re
+n = int(input())
+for _ in range(n):
+    line = input()
+    line = re.sub(r'(?<= )&&(?= )', 'and', line)
+    line = re.sub(r'(?<= )\|\|(?= )', 'or', line)
+    print(line)
